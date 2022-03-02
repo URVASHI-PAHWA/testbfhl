cf8a77c3bf7dd43d8d440ede97563d2e7ffaa1c5
Urvashi <urvashi0231.cse19@chitkara.edu.in>
2022-03-02 13:07:14 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+
+//body parser
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/bfhl', function(req, res){
+    let response = {
+        "is_success": true,
+        "user_id": "john_doe_17091999",
+        "email" : "john@xyz.com",
+        "roll_number":"ABCD123",
+        "numbers": req.body.data.filter(function(n){
+            return Number.isInteger(parseInt(n))? n : null;
+            }),
+        "alphabets": req.body.data.filter(function(n){
+            return !Number.isInteger(parseInt(n))? n : null;
+        }),
+
+    };
+    console.log(response.alphabets);
+    res.send(JSON.stringify(response));
+});
+
+app.listen(process.env.PORT||8080, function(){
+    console.log('listening on port 8080');
+});
